b1c9e2a8e04ad3e80e89f7cc17e1e1f644645285
perveevm <perveev_m@mail.ru>
2020-10-06 02:52:14 +0300

    Rename property

--- src/ru/strategy48/ejudge/util/XMLUtils.java
+++ src/ru/strategy48/ejudge/util/XMLUtils.java
@@ -215,7 +215,7 @@ public class XMLUtils {
         config.showFirstAC = Boolean.parseBoolean(mainConfig.getAttribute("first_ac"));
         config.showUsersWithoutRuns = Boolean.parseBoolean(mainConfig.getAttribute("empty_users"));
         config.showZeros = Boolean.parseBoolean(mainConfig.getAttribute("show_zero"));
-        config.showPenalty = Boolean.parseBoolean(mainConfig.getAttribute("show_penalty"));
+        config.showPenalty = !Boolean.parseBoolean(mainConfig.getAttribute("disable_penalty"));
 
         config.standingsName = standingsName.getTextContent();
         switch (standingsType.getTextContent()) {
