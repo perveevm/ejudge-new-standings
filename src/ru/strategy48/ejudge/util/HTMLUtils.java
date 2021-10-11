7a7bbef96bdeea948948db53c7adf4103b74c4f9
perveevm <perveev_m@mail.ru>
2021-10-12 01:19:38 +0300

    Added feature to search standings tables

--- src/ru/strategy48/ejudge/util/HTMLUtils.java
+++ src/ru/strategy48/ejudge/util/HTMLUtils.java
@@ -392,11 +392,13 @@ public class HTMLUtils {
                 "            <input type=\"text\" class=\"form-control\" placeholder=\"Поиск результатов\" id=\"search-field\">\n" +
                 "            <div class=\"input-group-append\">\n" +
                 "                <button class=\"btn btn-secondary\" type=\"button\" onclick=\"searchStandings()\">\n" +
-                "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-search\" viewBox=\"0 0 16 16\">\n" +
-                        "  <path d=\"M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z\"/>\n" +
-                        "</svg>"
-                +
+                "                    <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-search\" viewBox=\"0 0 16 16\">\n" +
+                "                        <path d=\"M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z\"/>\n" +
+                "                    </svg>\n" +
                 "                    <i class=\"fa fa-search\"></i>\n" +
+                "                </button>\n" +
+                "                <button class=\"btn btn-info btn-secondary\" type=\"button\" onclick=\"clearFilter()\">\n" +
+                "                    Сбросить фильтр\n" +
                 "                </button>\n" +
                 "            </div>\n" +
                 "        </div>\n" +
